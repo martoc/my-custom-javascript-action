799f06db8e843cbd3fc38b04fa3f6e4d19d615dd
martoc <martoc@users.noreply.github.com>
2024-01-26 11:08:46 +0000

    fix: Printed node version

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@
 /************************************************************************/
 var __webpack_exports__ = {};
 console.log('Mocking an AWS call via JS AWS SDK');
+console.log('Node.js version: ' + process.version);
 module.exports = __webpack_exports__;
 /******/ })()
 ;
